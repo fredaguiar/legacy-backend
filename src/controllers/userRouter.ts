836edd21e0250292416ca003a93526e792f815e3
Fred <fred.aguiar.teixeira@hotmail.com>
2024-07-15 17:20:38 -0300

    lifecheck notification via agenda

--- src/controllers/userRouter.ts
+++ src/controllers/userRouter.ts
@@ -1,5 +1,6 @@
 import express, { NextFunction, Request, Response } from 'express';
 import { Document } from 'mongoose';
+import Expo, { ExpoPushMessage, ExpoPushToken } from 'expo-server-sdk';
 import Agenda, { Job, JobAttributesData } from 'agenda';
 import User from '../models/User';
 
@@ -12,9 +13,30 @@ const userRouter = (_storage: AWS.S3, agenda: Agenda) => {
     lifeCheckInfo: { userId: string; firstName: string; lastName: string };
   }
 
+  // optionally providing an access token if you have enabled push security
+  const expo = new Expo({
+    // accessToken: process.env.EXPO_ACCESS_TOKEN,
+    useFcmV1: true, // this can be set to true in order to use the FCM v1 API
+  });
+
   agenda.define(SEND_NOTIFICATION, async (job: Job<ILifeCheck>) => {
     const info = job.attrs.data;
-    console.log('SEND NOTES job executed. lifeCheckInfo:', info);
+
+    const pushToken = process.env.EXPO_PUSH_TOKEN as ExpoPushToken;
+    if (!Expo.isExpoPushToken(process.env.EXPO_PUSH_TOKEN)) {
+      console.log(`Push token ${process.env.EXPO_PUSH_TOKEN} is not a valid Expo push token`);
+    }
+
+    const message: ExpoPushMessage = {
+      to: pushToken,
+      sound: 'default',
+      body: 'This is a test notification',
+    };
+
+    let ticket = await expo.sendPushNotificationsAsync([message]);
+    if (ticket[0]?.status !== 'ok') {
+      console.log(`Push token ticket error: ${ticket[0]?.status}`);
+    }
   });
 
   router.post('/updateUserProfile', async (req: Request, res: Response, next: NextFunction) => {
@@ -58,76 +80,6 @@ const userRouter = (_storage: AWS.S3, agenda: Agenda) => {
       await user.save();
 
       return res.json({ ...fieldsToUpdate, ...result });
-    } catch (error) {
-      next(error);
-    }
-  });
-
-  router.post('/updateLifeCheck', async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      // @ts-ignore
-      const userId = req.context.userId;
-
-      const user = await User.findById<Document & TUser>(userId).exec();
-      if (!user) {
-        return res.status(400).json({ message: 'User not found' });
-      }
-
-      let lifeCheck = {};
-      if (req.body.lifeCheck.shareCount) {
-        //update life check frequency
-        const {
-          lifeCheck: {
-            active,
-            shareCount,
-            shareCountType,
-            shareWeekdays,
-            shareTime,
-            shareCountNotAnswered,
-          },
-        } = req.body;
-        lifeCheck = {
-          ...user.lifeCheck,
-          shareCount,
-          shareCountType,
-          shareWeekdays,
-          shareTime,
-          shareCountNotAnswered,
-        };
-      } else {
-        lifeCheck = { ...user.lifeCheck, active: req.body.lifeCheck.active };
-      }
-      user.lifeCheck = lifeCheck;
-
-      await user.save();
-
-      // schedule notification
-      if (user.lifeCheck.active) {
-        const {
-          firstName,
-          lastName,
-          lifeCheck: { shareWeekdays, shareTime },
-        } = user;
-
-        const notificationData = { lifeCheckInfo: { userId, firstName, lastName } };
-        const [hours, minutes] = shareTime
-          ? shareTime.split(':').map((v: string) => parseInt(v))
-          : ['10', '00'];
-        const weekday = shareWeekdays
-          ?.map((day: string) => day.substring(0, 3).toUpperCase())
-          .join(',');
-        const cron = `${minutes} ${hours} * * ${weekday}`;
-
-        console.log('SEND NOTES cron:', cron);
-        console.log('SEND  notificationData:', notificationData);
-        // await agenda.start();
-        // await agenda.every(cron, SEND_NOTIFICATION, notificationData);
-      } else {
-        // remove from agenda
-        // agenda.cancel({ name: SEND_NOTIFICATION });
-      }
-
-      return res.json({ lifeCheck });
     } catch (error) {
       next(error);
     }
@@ -198,6 +150,76 @@ const userRouter = (_storage: AWS.S3, agenda: Agenda) => {
       console.log('getStorageInfo result', result);
 
       return res.json(result);
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  router.post('/updateLifeCheck', async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      // @ts-ignore
+      const userId = req.context.userId;
+
+      const user = await User.findById<Document & TUser>(userId).exec();
+      if (!user) {
+        return res.status(400).json({ message: 'User not found' });
+      }
+
+      let lifeCheck = {};
+      if (req.body.lifeCheck.shareCount) {
+        //update life check frequency
+        const {
+          lifeCheck: {
+            active,
+            shareCount,
+            shareCountType,
+            shareWeekdays,
+            shareTime,
+            shareCountNotAnswered,
+          },
+        } = req.body;
+        lifeCheck = {
+          ...user.lifeCheck,
+          shareCount,
+          shareCountType,
+          shareWeekdays,
+          shareTime,
+          shareCountNotAnswered,
+        };
+      } else {
+        lifeCheck = { ...user.lifeCheck, active: req.body.lifeCheck.active };
+      }
+      user.lifeCheck = lifeCheck;
+
+      await user.save();
+
+      // schedule notification
+      if (user.lifeCheck.active) {
+        const {
+          firstName,
+          lastName,
+          lifeCheck: { shareWeekdays, shareTime },
+        } = user;
+
+        const notificationData = { lifeCheckInfo: { userId, firstName, lastName } };
+        const [hours, minutes] = shareTime
+          ? shareTime.split(':').map((v: string) => parseInt(v))
+          : ['10', '00'];
+        const weekday = shareWeekdays
+          ?.map((day: string) => day.substring(0, 3).toUpperCase())
+          .join(',');
+        const cron = `${minutes} ${hours} * * ${weekday}`;
+
+        // console.log('SEND NOTES cron:', cron);
+        // console.log('SEND  notificationData:', notificationData);
+        await agenda.start();
+        await agenda.every(cron, SEND_NOTIFICATION, notificationData);
+      } else {
+        // remove from agenda
+        // agenda.cancel({ name: SEND_NOTIFICATION });
+      }
+
+      return res.json({ lifeCheck });
     } catch (error) {
       next(error);
     }
